b745ebde759c6ceaa5dff66e3198b626daa0d7d1
prax <praxtice@protonmail.com>
2019-02-24 11:58:40 -0700

    aaahhhhhh

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,7 @@
-const { Express } = require('express')
-var app = new Express()
+const routes = require('express').Router();
 
+routes.get('/', (req, res) => {
+  res.status(200).json({ message: 'Connected!' });
+});
+
+module.exports = routes;
